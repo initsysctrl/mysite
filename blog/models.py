881039f3c445e9bbc5615f9510959ca6bb2b7c18
yepeng <initsysctrl@163.com>
2021-04-19 08:22:23 +0800

    modle annotation

--- blog/models.py
+++ blog/models.py
@@ -19,13 +19,16 @@ class Blog(models.Model):
     author = models.CharField(max_length=20, verbose_name='作者', default='匿名')
     # 内容
     content = models.TextField(verbose_name='内容')
+    # 更新日期
     pub_tome = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
     modify_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
     # categories = [(0, 'python'), (1, 'java'), (2, 'c'), (3, 'c++')]
+    # 博客分类
     categor = models.IntegerField(
         choices=Categories.choices, default=Categories.python, verbose_name='内容分类')
+    # 插图
     illustration = models.ImageField(
         upload_to='images', null=True, verbose_name='插图')
-
+    # 简略实现
     def __str__(self) -> str:
         return self.title if self.title else "无标题"
